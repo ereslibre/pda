0c2b06d34734f7620c334c5c31d6a34bbce2df21
Rafael Fernández López <ereslibre@ereslibre.es>
2010-05-12 17:12:50 +0200

    Add test projects

---
+++ test2/fichero1.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the Foo project
+ * Copyright (C) 2010 Rafael Fernández López <ereslibre@ereslibre.es>
+ * Copyright (C) 2010 Jorge Olmos Mallol <thejofit@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef FICHERO1_H
+#define FICHERO1_H
+
+void foo()
+{
+	// This does foo
+}
+
+void bar()
+{
+	// This does bar
+}
+
+#endif //FICHERO1_H
