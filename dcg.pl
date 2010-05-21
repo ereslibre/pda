c6d674a06f3a1f0bc1b2f777b39efc39cbcd08e3
Rafael Fernández López <ereslibre@ereslibre.es>
2010-05-21 21:47:52 +0200

    rofl

--- dcg.pl
+++ dcg.pl
@@ -1,14 +1,34 @@
 findAllIncludes(F, [L | X]) :- findInclude(L, F, R), findAllIncludes(R, X).
 findAllIncludes([], []).
 
-findInclude(F) --> separadores, "#", sep0, "include", sep, fichero(F).
+findInclude(F) --> separadores0, "#", separadores0, "include", separadores0, fichero(F).
 
-separadores --> " " | "\n" | "\t", separadores.
-separadores --> [].
+findFunctionsProt([F | [R]], (X, Y)) --> separadores0, tipo(F), separadores, nombre(R), separadores0, "(", listaParam, ")", separadores0, ";",
+                                         { name(X, R), Y = 0 }.
+
+listaParam --> [].
+
+nombre([C | R]) --> [C], { is_letter(C) }, nombreRes(R).
+nombreRes([C | R]) --> [C], { is_char(C) }, nombreRes(R).
+nombreRes([]) --> [].
+
+tipo(F) --> nombre(F), separadores0, asteriscos0.
+
+ is_letter(X) :- X >= "a", X =< "z".
+ is_letter(X) :- X >= "A", X =< "Z".
+
+ is_char(X) :- (is_letter(X) | (X >= "0", X =< "9")). 
+
+asteriscos0 --> "*", asteriscos0.
+asteriscos0 --> [].
+
+separadores0 --> (" " | "\t" | comentarios), separadores0.
+separadores0 --> [].
+
+separadores --> " " , separadores0.
 
 sep0 --> " ", sep0.
-sep0 --> [].
-
+sep0 --> []. 
 sep --> " ", sep0.
 
 fichero(F) --> "<", cadena(R), { name(F, R) }, ">".
@@ -18,24 +38,21 @@ cadena([C | R]) --> [C], { not(member(C, [">", "\""])) }, cadena1(R).
 cadena1([C | R]) --> [C], { not(member(C, [">", "\""])) }, cadena1(R).
 cadena1([]) --> [].
 
-
-
 % Reglas del preprocesado del fichero
 % Eliminan comentarios y saltos inecesarios. Al finalizar obtenemos un fichero
 % donde cada salto de línea es precedido por ;
 
-%preprocesador(F) --> comentarios, linea(F).
-linea(F) --> nosep(R), { name(F, R) }.
+% preprocesador(F) --> sep0, linea(F).
+% linea(F) --> nosep(R), { name(F, R) }.
+% 
+% nosep([C | R]) --> [C], { not(member(C, ":;\n")) }, !, nosep1(R).
+% nosep(R) -->[_], nosep1(R).
+% nosep1([C | R]) --> [C], { not(member(C, ":;\n")) }, !, nosep1(R).
+% nosep1(R) --> [_], nosep1(R).
+% nosep1([]) --> [].
 
-nosep([C | R]) --> [C], { not(member(C, ":;")) }, !, nosep1(R).
-nosep(R) -->[_], nosep1(R).
-nosep1([C | R]) --> [C], { not(member(C, ":;")) }, !, nosep1(R).
-nosep1(R) --> [_], nosep1(R).
-nosep1([]) --> [].
-
-comentarios --> comentarioLinea | comentarioLineas | [].
+comentarios --> comentarioLinea | comentarioLineas.
 comentarioLineas --> "/*", comentario, "*/".
 comentarioLinea --> "//", comentario, "\n".
-comentario --> _, comentario.
+comentario --> [_]  , comentario.
 comentario --> [].
-
